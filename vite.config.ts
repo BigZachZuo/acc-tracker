c23e03dd2601429720d9ac9768419b33ce09456d
BigZachZuo <zuoyi186@126.com>
2025-12-01 01:10:50 +0800

    revert to Netlify version

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  // We cast process to any to avoid TypeScript errors since we removed @types/node
   const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
     plugins: [react()],
     define: {
-      // Expose API_KEY to the client
+      // Expose API_KEY to the client for Netlify/Vercel
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
   };
