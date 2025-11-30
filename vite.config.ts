8d0d83abf45bf3b07b84de4ede56365131e3b39a
BigZachZuo <zuoyi186@126.com>
2025-12-01 00:45:30 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  // Cast process to any to fix TS error: Property 'cwd' does not exist on type 'Process'
   const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
